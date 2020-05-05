bdbae9c6a8ef9de8fb286bb2107f45d9e389a11d
envincebal <vincentyan1@gmail.com>
2020-05-04 18:23:24 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+// Setup empty JS object to act as endpoint for all routes
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+projectData = {};
+
+// Require Express to run server and routes
+
+// Start up an instance of app
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
